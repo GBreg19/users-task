ba7bc07119c5ab91d7a649616cf049e4db000eb9
Giorgi Bregvadze <giorgi.bregvadze.1998@gmail.com>
2023-10-06 23:12:05 +0400

    deploy update

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from "./store/store.ts";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/users-task/",
     element: <UsersTable />,
     errorElement: <ErrorPage />,
   },
